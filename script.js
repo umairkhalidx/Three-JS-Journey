a4bb6a00a401ff8e39c8d6a5bb6dfe7f0c1641a9
Umair Khalid <umair32khalid@gmail.com>
2026-07-07 23:12:37 +0500

    Custom Model Created and Imported via Belender

--- script.js
+++ script.js
@@ -1,183 +1,144 @@
-//Libraries
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import GUI from 'lil-gui'
-import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
 
-
-////////////////////Debug////////////////////
-
-const gui = new GUI({
-    width: 300,
-    title: "Tweaks Menu",
-    closeFolders: true
-})
-gui.close()
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Variables////////////////////
-
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Models
+//  */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./static/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+    './static/models/burger.glb',
+    (gltf) =>
+    {
+        scene.add(gltf.scene)
+    }
+)
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(50, 50),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-//////////////////////////////////////////////////
-
-
-
-////////////////////Renderer////////////////////
-
-const renderer = new THREE.WebGLRenderer({
-    canvas:canvas,
-})
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Scene////////////////////
-
-const scene = new THREE.Scene()
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Camera////////////////////
-
-const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 100 )
-camera.position.set(0, 0, 10)
-camera.lookAt(0, 0, 0)
-scene.add(camera)
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Controls////////////////////
-
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Textures////////////////////
-
-const textureLoader = new THREE.TextureLoader()
-
-// Wall
-const colorTexture = textureLoader.load('./static/textures/laminatedFloor/laminate_floor_02_diff_1k.jpg')
-const armTexture = textureLoader.load('./static/textures/laminatedFloor/laminate_floor_02_arm_1k.jpg')
-const normalTexture = textureLoader.load('./static/textures/laminatedFloor/laminate_floor_02_nor_gl_1k.jpg')
-const displacementTexture = textureLoader.load('./static/textures/laminatedFloor/laminate_floor_02_disp_1k.jpg')
-
-colorTexture.colorSpace = THREE.SRGBColorSpace
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Objects////////////////////
-
-const basicMaterial = new THREE.MeshBasicMaterial({
-    map: colorTexture,
-    aoMap: armTexture,
-    roughnessMap: armTexture,
-    metalnessMap: armTexture,
-    normalMap: normalTexture,
-    displacementMap: displacementTexture,
-    displacementScale: 0.3,
-    displacementBias: - 0.2,
- })
-const TorusKnot = new THREE.TorusGeometry(2.4, 0.7, 128, 32)
-const basicMesh = new THREE.Mesh(TorusKnot, basicMaterial)
-basicMesh.position.set(0, 0, 0)
-scene.add(basicMesh)
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Lights////////////////////
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.set(2, 2, 2)
-
-scene.add(directionalLight)
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Helper////////////////////
-
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
-// const cameraHelper = new THREE.CameraHelper(camera)
-// scene.add(cameraHelper)
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Animations////////////////////
-const clock = new THREE.Clock()
-
-const tick = () =>{
-    
-    //Get Elaspsed Time
-    const elapsedTime = clock.getElapsedTime()
-
-    //Update Controls
-    controls.update()
-
-    //Animate the objects
-    basicMesh.rotation.y += 0.01
-    basicMesh.rotation.x += 0.005
-
-    //Rerender the scene
-    renderer.render(scene,camera)
-
-    //Request Animation
-    window.requestAnimationFrame(tick)
-
-}
-tick()
-
-//////////////////////////////////////////////////
-
-
-
-////////////////////Event Listners////////////////////
-window.addEventListener("resize", () =>{
-
+window.addEventListener('resize', () =>
+{
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    //Update Camera
-    camera.aspect = sizes.width/sizes.height
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    //Update Controls
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(- 8, 4, 8)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 1, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    if(mixer)
+    {
+        mixer.update(deltaTime)
+    }
+
+    // Update controls
     controls.update()
 
-    //Update Renderer
-    renderer.setSize(sizes.width,sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+    // Render
+    renderer.render(scene, camera)
 
-} )
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-//////////////////////////////////////////////////
+tick()
